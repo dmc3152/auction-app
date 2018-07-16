46a563aaec4092d07c8a45722de7fb4734eaed83
Daniel Cook <dmc3152@yahoo.com>
2018-07-15 17:46:57 -0700

    initialized auction app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/application/app.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ProductDetailComponent } from './components/product-detail/product-detail.component';
+import { ProductItemComponent } from './components/product-item/product-item.component';
+import { SearchComponent } from './components/search/search.component';
+import { StarsComponent } from './components/stars/stars.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarouselComponent,
+    FooterComponent,
+    HomeComponent,
+    NavbarComponent,
+    ProductDetailComponent,
+    ProductItemComponent,
+    SearchComponent,
+    StarsComponent
   ],
   imports: [
     BrowserModule
