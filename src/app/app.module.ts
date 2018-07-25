bbe27257a388785df3cdf626664bc172a15b0b78
Daniel Cook <dmc3152@yahoo.com>
2018-07-24 22:19:39 -0700

    added filter pipe to home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from "@angular/router";
 import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 import { AppComponent } from './components/application/app.component';
 import { CarouselComponent } from './components/carousel/carousel.component';
@@ -12,6 +13,8 @@ import { ProductDetailComponent } from './components/product-detail/product-deta
 import { ProductItemComponent } from './components/product-item/product-item.component';
 import { SearchComponent } from './components/search/search.component';
 import { StarsComponent } from './components/stars/stars.component';
+
+import { FilterPipe } from "./pipes/filter.pipe";
 
 import { ProductService } from "./services/product.service";
 
@@ -25,7 +28,8 @@ import { ProductService } from "./services/product.service";
     ProductDetailComponent,
     ProductItemComponent,
     SearchComponent,
-    StarsComponent
+    StarsComponent,
+    FilterPipe
   ],
   imports: [
     BrowserModule,
@@ -33,11 +37,13 @@ import { ProductService } from "./services/product.service";
       { path: '', component: HomeComponent },
       { path: 'products/:productId', component: ProductDetailComponent }
     ]),
-    CarouselModule
-    ],
-      providers: [
-        ProductService
-      ],
-      bootstrap: [AppComponent]
+    CarouselModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [
+    ProductService
+  ],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from "@angular/forms";
 import { Product, ProductService } from '../../services/product.service';
+import { FilterPipe } from "../../pipes/filter.pipe";
+import { debounceTime } from "rxjs/operators";
 
 @Component({
   selector: 'auction-home-page',
@@ -8,9 +11,19 @@ import { Product, ProductService } from '../../services/product.service';
 })
 export class HomeComponent implements OnInit {
   products: Product[] = [];
+  titleFilter: FormControl = new FormControl();
+  filterCriteria: string;
 
   constructor(private productService: ProductService) {
     this.products = this.productService.getProducts();
+    this.titleFilter.valueChanges
+      .pipe(
+        debounceTime(100)
+      )
+      .subscribe(
+        value => this.filterCriteria = value,
+        error => console.error(error)
+      );
   }
 
   ngOnInit() {
